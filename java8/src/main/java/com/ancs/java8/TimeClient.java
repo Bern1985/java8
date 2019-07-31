466b70b27d710344075f6d1ccef15b30f1535e98
bern <zhanghua850127@126.cn>
2019-07-31 09:51:16 +0800

    default methods

--- java8/src/main/java/com/ancs/java8/TimeClient.java
+++ java8/src/main/java/com/ancs/java8/TimeClient.java
@@ -8,4 +8,18 @@ public interface TimeClient {
     void setDateAndTime(int day, int month, int year,
                         int hour, int minute, int second);
     LocalDateTime getLocalDateTime();
+
+    static ZoneId getZoneId (String zoneString) {
+        try {
+            return ZoneId.of(zoneString);
+        } catch (DateTimeException e) {
+            System.err.println("Invalid time zone: " + zoneString +
+                    "; using default time zone instead.");
+            return ZoneId.systemDefault();
+        }
+    }
+
+    default ZonedDateTime getZonedDateTime(String zoneString) {
+        return ZonedDateTime.of(getLocalDateTime(), getZoneId(zoneString));
+    }
 }
