589a9241bb9c6dde13892102cd879860c03186ed
bern <zhanghua850127@126.cn>
2019-08-01 16:45:44 +0800

    Using Predicate on a collection

--- java8/src/main/java/predicateExample/Employee.java
+++ java8/src/main/java/predicateExample/Employee.java
@@ -16,7 +16,45 @@ public class Employee {
     private String firstName;
     private String lastName;
 
-    //Please generate Getter and Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
     //To change body of generated methods, choose Tools | Templates.
     @Override
